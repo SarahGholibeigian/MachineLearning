f546739f5da2db6a169276e0045429e0cbac01cb
M-Ghodrat <ghodrat@ualberta.ca>
2021-04-06 18:35:35 -0600

    update Chatbot

---
+++ Chatbot/main.py
@@ -0,0 +1,126 @@
+import nltk
+# nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+# avoid preprocessings if it has already done
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = [] # tokenized words in patterns of all tags
+    labels = [] # distinct tags
+    docs_x = [] # list of all words
+    docs_y = [] # list of tags associated with the words in docs_x
+    ignore_letters = [",", "!", ".", ":", "?"]
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w not in ignore_letters] # get the stem of words
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    # One-Hot Endocing
+    # Example of OHE on labels: (the same is true on words)
+    # labels: ['age', 'goodbye', 'greeting', 'hours', 'name', 'shop']
+    # example of an encoded pattern: [1, 1, 0, 0, 2, 0], numbers shows frequency
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+# tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+# load the model if it already exists, otherwise train the model
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+# prediction
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
+        else:
+            print("I didn't get that, try again!")
+
+chat()
+
+
